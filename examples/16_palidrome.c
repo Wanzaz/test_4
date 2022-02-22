7a74c14c6abdd90e2b2ced715890bac28909637a
Wanzaz <ondra.pazourek@gmail.com>
2022-02-22 16:46:04 +0100

    16_palidrome.c create {one of the hardest examples in this test}

---
+++ examples/16_palidrome.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+16. Napište funkci, která vezme øetìzec a vrátí 1, pokud je  øetìzec palindromem, jinak vrátí 0? (oko, radar).
+16. Write a function that takes a string and returns 1 if the string is a palindrome, otherwise it returns 0 (eye, radar).
+*/
+
+void reverse(char *source);
+int palidrome(char *normal, char *reverse);
+void copy(char *source,char *target);
+
+int main()
+{
+    char str[40];
+    printf("Enter something to str: ");
+    scanf("%39s", str);
+
+    char str2[40];
+    
+    copy(str, str2);
+    reverse(str);
+    
+    if(palidrome(str2, str)==0)
+        printf("The word is palidrome");
+    else
+        printf("The word is not palidrome");
+
+    return 0;
+}
+
+void reverse(char *source)
+{
+    int i;
+    int length = strlen(source);
+    for(i=0;i <= length;i++)
+    {
+        source[i] = source[length - 1 - i];
+    }
+    source[i] = '\0';
+}
+
+void copy(char *source,char *target)
+{
+    int i;
+    for(i=0;source[i]!='\0';i++)
+    {
+        target[i] = source[i];
+    }
+    target[i]='\0';
+}
+
+int palidrome(char *normal, char *reverse)
+{
+    if(strcmp(normal, reverse)==0)
+        return 0;
+    return 1;
+}
